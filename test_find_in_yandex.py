89cdbc0439db7f003a8602a4390b1f621a71e4f2
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-08-01 16:12:20 +0500

    Выполнил первое задание

---
+++ test_find_in_yandex.py
@@ -0,0 +1,37 @@
+from YandexPage import SearchForm, YandexLocators
+
+
+def test_check_yandex_search(browser):
+    """Открывает главную страницу yandex и проверяет наличие поля поиска"""
+    yandex_page = SearchForm(browser)
+    yandex_page.open_main_page()
+    assert yandex_page.is_element_exist(YandexLocators.LOCATOR_YANDEX_SEARCH_FIELD)
+
+
+def test_check_suggest_table(browser):
+    """Проверяет, что при вводе слова 'тензор' появляется таблица с подсказками запроса"""
+    yandex_page = SearchForm(browser)
+    yandex_page.open_main_page()
+    yandex_page.enter_word("тензор")
+    assert yandex_page.is_element_exist(YandexLocators.LOCATOR_YANDEX_SUGGEST_TABLE)
+
+
+def test_check_results_table(browser):
+    """Проверяет, что при вводе слова 'тензор' в поисковик и нажатии Enter, появляется
+        страница с таблицей результатов поиска"""
+    yandex_page = SearchForm(browser)
+    yandex_page.open_main_page()
+    yandex_page.enter_word("тензор")
+    yandex_page.press_enter_to_find_results()
+    assert yandex_page.is_element_exist(YandexLocators.LOCATOR_YANDEX_RESULT_TABLE)
+
+
+def test_check_first_element_in_results(browser):
+    """Проверяет, что в таблице результатов поиска по слову 'тензор' первой записью
+        является ссылка на официальный сайт Тензора"""
+    yandex_page = SearchForm(browser)
+    yandex_page.open_main_page()
+    yandex_page.enter_word("тензор")
+    yandex_page.press_enter_to_find_results()
+    link = yandex_page.find_element(YandexLocators.LOCATOR_YANDEX_FIRST_RESULT)
+    assert link.get_attribute('href') == "https://tensor.ru/"
